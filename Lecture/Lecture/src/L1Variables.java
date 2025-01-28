58eabab2475b1653633012c38bca6537d0c1f7e8
snehabhosale1770 <snehabhosale683@gmail.com>
2025-01-28 14:44:23 +0530

    1st code

---
+++ Lecture/Lecture/src/L1Variables.java
@@ -0,0 +1,20 @@
+public class L1Variables {
+    public static void main(String[] args) {
+        int yourNumber;
+        yourNumber=87;
+        System.out.println(yourNumber);
+        yourNumber=88;
+        System.out.println(yourNumber);
+
+        boolean isvegetarian = true;
+        System.out.println(isvegetarian);
+
+        float myFloat = 5;
+        double myDouble = 9.8888;
+        System.out.println(myDouble);
+        System.out.println(myFloat);
+
+        String wishes = "Hello Students";
+        System.out.println(wishes);
+    }
+}
