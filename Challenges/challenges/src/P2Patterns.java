d2e4621f3f8c6d3c6e2c0820a50295fd8b78dbbe
snehabhosale1770 <snehabhosale683@gmail.com>
2025-01-28 14:28:47 +0530

    2nd code

---
+++ Challenges/challenges/src/P2Patterns.java
@@ -0,0 +1,24 @@
+public class P2Patterns {
+    public static void main(String[] args) {
+        System.out.println("Right Half Pattern");
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+
+        System.out.println("Reverse Right Half Pattern");
+        System.out.println("* * * * *");
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("* ");
+
+        System.out.println("Left Half Pattern");
+        System.out.println("        *");
+        System.out.println("      * *");
+        System.out.println("    * * *");
+        System.out.println("  * * * *");
+        System.out.println("* * * * *");
+    }
+}
