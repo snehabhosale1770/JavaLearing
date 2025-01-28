dae990d3994e02a9ce83497c4f2e2dbb37cc5f86
snehabhosale1770 <snehabhosale683@gmail.com>
2025-01-28 14:36:07 +0530

    5th code

---
+++ Challenges/challenges/src/P5Escapepatterns.java
@@ -0,0 +1,15 @@
+public class P5Escapepatterns {
+    public static void main(String[] args) {
+        System.out.println(" *\n * * \n * * * \n * * * * \n * * * * *");
+        // Right-Half pryamid
+
+
+
+        System.out.println(" * * * * * \n * * * * \n * * * \n * * \n *");
+        // Reverse-Right-Half pryamid
+
+
+        System.out.println("          * \n        * * \n      * * * \n    * * * * \n  * * * * *");
+        // Left-Half pryamid
+    }
+}
