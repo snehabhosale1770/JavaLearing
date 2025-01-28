b223de17e33037f572c3a295a3926e2dccf91a9b
snehabhosale1770 <snehabhosale683@gmail.com>
2025-01-28 21:27:58 +0530

    changes in 6th and 7th code

--- Challenges/challenges/src/P6UserInput.java
+++ Challenges/challenges/src/P6UserInput.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class P6UserInput {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter Your Name :");
+        System.out.print("Enter Your Name : ");
         String Name = input.nextLine();
-        System.out.println("Welcome" + Name + "to KG Coding");
+        System.out.println("Welcome " + Name + " to KG Coding");
     }
 }
--- Challenges/challenges/src/P7AddTwoNum.java
+++ Challenges/challenges/src/P7AddTwoNum.java
@@ -3,13 +3,20 @@ import java.util.Scanner;
 public class P7AddTwoNum {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter First Numbers :");
+        System.out.print("Enter First Numbers : ");
         int num1 = input.nextInt();
-        System.out.print("Enter Second Numbers :");
+        System.out.print("Enter Second Numbers : ");
         int num2 = input.nextInt();
         int sum = num1 + num2;
-        System.out.println("Addition is :" + sum);
+        System.out.println("Addition of " + num1 + " and " + num2 + " is : " + sum);
 
+        int sub = num1 - num2;
+        System.out.println("Subtraction of " + num1 + " and " + num2 + " is : " + sub);
 
+        int mul = num1 * num2;
+        System.out.println("Multiplication of " + num1 + " and " + num2 + " is : " + mul);
+
+        int div = num1 / num2;
+        System.out.println("Division of " + num1 + " and " + num2 + " is : " + div);
     }
 }
