2e0f4cecd0b673036733006b87bac855eeaa8872
snehabhosale1770 <snehabhosale683@gmail.com>
2025-01-29 23:02:03 +0530

    P13 Simple Interest code

---
+++ Challenges/challenges/src/P13SimpleInterest.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class P13SimpleInterest {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Simple Interest Program ");
+        System.out.print("Enter Principle : ");
+        int P = input.nextInt();
+        System.out.print("Enter Time : ");int T = input.nextInt();
+        System.out.print("Enter Rate : ");
+        int R = input.nextInt();
+
+        float simpleInterest = (P*T*R)/100;
+        System.out.println("Simple Interest is : " + simpleInterest);
+    }
+}
