303028408762c599024aee9dcb6ee39ceebd2b71
snehabhosale1770 <snehabhosale683@gmail.com>
2025-01-30 21:27:12 +0530

    L4 Rational Operator program code

---
+++ Lecture/Lecture/src/L4RationalOperator.java
@@ -0,0 +1,18 @@
+public class L4RationalOperator {
+    public static void main(String[] args) {
+
+        boolean isSeniorCitizen = false;
+        boolean isAdult = true;
+
+        if(isSeniorCitizen){
+            System.out.println("Hello senior citizen");
+        }
+        else if(isAdult){
+            System.out.println("Hello Adult");
+        }
+        else {
+            System.out.println("Hello child");
+           }
+        }
+    }
+
