92db5691fba334ac443c2746d722801af136a253
snehabhosale1770 <snehabhosale683@gmail.com>
2025-02-02 14:49:33 +0530

    L5 While Loop changes in java code

--- Lecture/Lecture/src/L5WhileLoop.java
+++ Lecture/Lecture/src/L5WhileLoop.java
@@ -1,17 +1,27 @@
+import java.util.Scanner;
+
 public class L5WhileLoop {
     public static void main(String[] args) {
 
-//        int num = 1; // initialization
-//        while(num <= 10){  // condition
-//            System.out.println(num);  // actual work
-//            num = num + 1; // updating the condition
-//        }
+        int num = 1; // initialization
+        while(num <= 10){  // condition
+           System.out.println(num);  // actual work
+           num = num + 1; // updating the condition
+       }
 
         // Printing The Reverse Numbers
         int count = 10;
         while (count >= 1){
             System.out.println(count);
-            count = count - 1;
+           count -= 1;
+       }
+
+        Scanner input = new Scanner(System.in);
+        int i = 0;
+        while(i <= 5){
+            int inp = input.nextInt();
+            System.out.println("Number is " + inp);
+            i++;
         }
     }
 }
