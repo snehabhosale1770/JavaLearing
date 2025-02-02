7958b4c4419c13c57a5df056050e92f8c0f774b8
snehabhosale1770 <snehabhosale683@gmail.com>
2025-02-02 14:13:15 +0530

    L5 Comments in java code

---
+++ Lecture/Lecture/src/L5CommentsInJava.java
@@ -0,0 +1,14 @@
+public class L5CommentsInJava {
+    /**
+     * main
+     * @param args
+     */
+    public static void main(String[] args) {
+        // here printing the hello world message / single line comment
+        System.out.println("Hello World");
+        /*
+        here printing the:
+                 Multiline comment
+         */
+    }
+}
