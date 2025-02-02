e045b8e3fe06d8694797421467410d15032a816e
snehabhosale1770 <snehabhosale683@gmail.com>
2025-02-02 14:40:51 +0530

    L5 While Loop in java code

---
+++ Lecture/Lecture/src/L5WhileLoop.java
@@ -0,0 +1,17 @@
+public class L5WhileLoop {
+    public static void main(String[] args) {
+
+//        int num = 1; // initialization
+//        while(num <= 10){  // condition
+//            System.out.println(num);  // actual work
+//            num = num + 1; // updating the condition
+//        }
+
+        // Printing The Reverse Numbers
+        int count = 10;
+        while (count >= 1){
+            System.out.println(count);
+            count = count - 1;
+        }
+    }
+}
